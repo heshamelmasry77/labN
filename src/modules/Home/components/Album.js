4228dd2ff18bbe251d1fa0ea6e56a9571045b4a5
Hesham El Masry <heshamelmasry@outlook.com>
2017-12-10 18:59:41 +0200

    adding dummy images

--- src/modules/Home/components/Album.js
+++ src/modules/Home/components/Album.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import '../../../Album.css';
 import { Grid, Thumbnail, Col, Row } from 'react-bootstrap';
-import ThumbnailImageOne from '../../../assets/album-images/img-1.png'; // Tell Webpack this JS file uses this image
-import ThumbnailImageTwo from '../../../assets/album-images/img-2.png'; // Tell Webpack this JS file uses this image
-import ThumbnailImageThree from '../../../assets/album-images/img-3.png'; // Tell Webpack this JS file uses this image
-import ThumbnailImageFour from '../../../assets/album-images/img-4.png'; // Tell Webpack this JS file uses this image
+import ThumbnailImageOne from '../../../assets/album-images/img-1.jpg'; // Tell Webpack this JS file uses this image
+import ThumbnailImageTwo from '../../../assets/album-images/img-2.jpg'; // Tell Webpack this JS file uses this image
+import ThumbnailImageThree from '../../../assets/album-images/img-3.jpg'; // Tell Webpack this JS file uses this image
+import ThumbnailImageFour from '../../../assets/album-images/img-4.jpg'; // Tell Webpack this JS file uses this image
+import ThumbnailImageFive from '../../../assets/album-images/img-5.jpg'; // Tell Webpack this JS file uses this image
+import ThumbnailImageSix from '../../../assets/album-images/img-6.jpg'; // Tell Webpack this JS file uses this image
+import ThumbnailImageSeven from '../../../assets/album-images/img-7.jpg'; // Tell Webpack this JS file uses this image
+import ThumbnailImageEight from '../../../assets/album-images/img-8.jpg'; // Tell Webpack this JS file uses this image
 
 class Album extends Component {
   render() {
@@ -43,7 +47,7 @@ class Album extends Component {
           <Row>
             <Col xs={6} md={3} className="thumbnail-container">
               <a href="">
-                <Thumbnail href="" alt="" src={ThumbnailImageFour} />
+                <Thumbnail href="" alt="" src={ThumbnailImageFive} />
                 <h4 className="card-text text-center">
                   Neuro Linguistic<br />
                   Search
@@ -52,19 +56,19 @@ class Album extends Component {
             </Col>
             <Col xs={6} md={3} className="thumbnail-container">
               <a href="">
-                <Thumbnail href="" alt="" src={ThumbnailImageThree} />
+                <Thumbnail href="" alt="" src={ThumbnailImageSix} />
                 <h4 className="card-text text-center">Video</h4>
               </a>
             </Col>
             <Col xs={6} md={3} className="thumbnail-container">
               <a href="">
-                <Thumbnail href="" alt="" src={ThumbnailImageTwo} />
+                <Thumbnail href="" alt="" src={ThumbnailImageSeven} />
                 <h4 className="card-text text-center">Neuromarketing</h4>
               </a>
             </Col>
             <Col xs={6} md={3} className="thumbnail-container">
               <a href="">
-                <Thumbnail href="" alt="" src={ThumbnailImageOne} />
+                <Thumbnail href="" alt="" src={ThumbnailImageEight} />
                 <h4 className="card-text text-center">
                   Analysis &<br />
                   Improvement
